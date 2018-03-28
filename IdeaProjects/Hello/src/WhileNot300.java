6f5cb75ec8e2d379d14de8274699b6bc630cce7a
Igor <ik19890104@gmail.com>
2018-03-28 17:46:09 +0300

    1

---
+++ IdeaProjects/Hello/src/WhileNot300.java
@@ -0,0 +1,16 @@
+public class WhileNot300 {
+
+public void setWhileNot300(){
+
+    int a=1;
+    int b=1;
+    while(a*b<300){
+
+        System.out.print(" "+a);
+        b=a;
+        a=a+3;
+
+    }
+
+}
+}
