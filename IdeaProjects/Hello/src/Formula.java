3cba0244ec0f46ea13e531a167858c966040e0ed
Igor <ik19890104@gmail.com>
2018-03-28 17:44:21 +0300

    1

---
+++ IdeaProjects/Hello/src/Formula.java
@@ -0,0 +1,22 @@
+public class Formula {
+
+
+        private int m,n,c;
+
+        public void setForm(int a, int b){
+            m=a;
+            n=b;
+            c=4*(m+n-1)/2;
+        }
+            public int getForm(){
+            return(c);
+            }
+                }
+
+
+
+
+
+
+
+
