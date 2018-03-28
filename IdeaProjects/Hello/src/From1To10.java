c814a7b0649bcf1548898b02f5f21b36a2719f10
Igor <ik19890104@gmail.com>
2018-03-28 17:46:16 +0300

    1

---
+++ IdeaProjects/Hello/src/From1To10.java
@@ -0,0 +1,37 @@
+
+
+public class From1To10 {
+
+
+public void setFrom1To10(){
+    int a=1;
+    while(a<11){
+
+        if(a%2==0){
+            System.out.println("a равное "+a+" четное");
+        }
+        else{
+            System.out.println("a равное "+a+" нечетное");
+        }
+        a++;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
